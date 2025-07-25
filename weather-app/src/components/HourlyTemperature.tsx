1900129d7e076e517743ae5e425ed9a4e588f711
sakshamgiri@2023 <sakshamgiri500@gmail.com>
2025-07-25 21:58:37 +0530

    added restaurant file

--- weather-app/src/components/HourlyTemperature.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HourlyTemperature = () => {
-    return (
-        <div>HourlyTemperature</div>
-    )
-}
-
-export default HourlyTemperature
--- weather-app/src/pages/CityPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CityPage = () => {
-    return (
-        <div>CityPage</div>
-    )
-}
-
-export default CityPage
